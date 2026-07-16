dd7148ec4b81d5f3a4ee398482dffcbca649cfe2
polgilomella <polgilomella96@gmail.com>
2026-07-16 08:51:12 +0200

    Forzar actualización de caché

--- sw.js
+++ sw.js
@@ -1,5 +1,6 @@
-const CACHE='fitbloq-v2';
+const CACHE='fitbloq-v3';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./enhancements.js','./assets/leaflet/leaflet.css','./assets/leaflet/leaflet.js','./manifest.webmanifest'];
-self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS))));
+self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)))});
+self.addEventListener('activate',event=>{event.waitUntil(self.clients.claim())});
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('fitbloq-')&&k!==CACHE).map(k=>caches.delete(k))))));
 self.addEventListener('fetch',event=>event.respondWith(caches.match(event.request).then(hit=>hit||fetch(event.request))));
