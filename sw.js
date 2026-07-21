7e0206aa91346a36f2e14714b7c8420e9dfca076
polgilomella <polgilomella96@gmail.com>
2026-07-21 07:25:57 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='fitbloq-v5';
+const CACHE='fitbloq-v6-force-custom-strength';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./enhancements.js','./assets/leaflet/leaflet.css','./assets/leaflet/leaflet.js','./manifest.webmanifest'];
 self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)))});
 self.addEventListener('activate',event=>{event.waitUntil(self.clients.claim())});
