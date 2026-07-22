992de4ad64b63879a328c06230c8f448c500ac95
polgilomella <polgilomella96@gmail.com>
2026-07-22 12:08:53 +0200

    Forzar caché editor por días

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='fitbloq-v11-day-editor';
+const CACHE='fitbloq-v12-day-editor-force';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./enhancements.js','./assets/leaflet/leaflet.css','./assets/leaflet/leaflet.js','./manifest.webmanifest'];
 self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)))});
 self.addEventListener('activate',event=>{event.waitUntil(self.clients.claim())});
