5cfe430e4e755bc732c55f5c82044212840eea17
polgilomella <polgilomella96@gmail.com>
2026-07-22 13:32:59 +0200

    cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='fitbloq-v14-sport-metrics';
+const CACHE='fitbloq-v15-force-refresh';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./enhancements.js','./assets/leaflet/leaflet.css','./assets/leaflet/leaflet.js','./manifest.webmanifest'];
 self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)))});
 self.addEventListener('activate',event=>{event.waitUntil(self.clients.claim())});
